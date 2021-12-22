32a4e8a6de5c41e4f665571000eb121a54cb7eb9
zgMrt <96428690+zgMrt@users.noreply.github.com>
2021-12-22 21:44:47 +0800

    数据的存储

---
+++ 数据存储/数据存储/test.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	return 0;
+}
+//int but()
+//{
+//	int a = 1;
+//	char* p = (char*)&a;
+//	if (*p == 1)
+//		return 1;
+//	else
+//		return 0;
+//}
+//int but()
+//{
+//	int a = 1;
+//	return *(char*)&a;
+//}
+//int main()
+//{
+//	int ret = but();
+//	if (ret == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	char* p = (char*)&a;
+//	if (*p == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//	return 0;
+//}
