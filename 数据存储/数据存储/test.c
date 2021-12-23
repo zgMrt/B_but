b8aa02d3949a0ca273502196207380ef82769553
zgMrt <96428690+zgMrt@users.noreply.github.com>
2021-12-23 21:07:02 +0800

    数据存储的方式测试

--- 数据存储/数据存储/test.c
+++ 数据存储/数据存储/test.c
@@ -2,8 +2,45 @@
 #include<stdio.h>
 int main()
 {
+	int n = 9;
+	float* pFloat = (float*)&n;
+	printf("n的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+	*pFloat = 9.0;
+	printf("num的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
 	return 0;
 }
+//unsigned char i = 0;
+//int main()
+//{
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello world\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char a[1000];
+//	//int a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d", strlen(a));
+//	return 0;
+//}
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//	}
+//	return 0;
+//}
 //int but()
 //{
 //	int a = 1;
